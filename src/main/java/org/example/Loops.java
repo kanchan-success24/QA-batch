fc30e88b905a2653de27750ba93938598b901c5a
Kanchan Kumari <kanchank211195@gmail.com>
2025-07-02 11:34:48 +0530

    first commit

---
+++ src/main/java/org/example/Loops.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Loops {
+    public static void main(String[] args) {
+      //  int sum=0;
+       // for (int i =1; i<=10; i++){
+           // sum=sum+i;
+          //  System.out.println(sum);
+       // }
+
+      // int sum=1;
+      //  for (int i =1; i<=10; i++){
+         //  sum=sum*i;}
+          // System.out.println(sum);
+        //for (int i=1; i<=10; i++){
+           // if(i%2==0) {
+              //  System.out.println(i);
+
+            //}}
+
+         int i=1;
+         while(i<=10){
+             System.out.println(i);
+             i++;
+         }
+
+    }
+
+    }
+
---
+++ src/main/java/prac.java
@@ -0,0 +1,84 @@
+public class prac {
+    public static void main(String[] args) {
+       // int marks= 3;
+        //marks++;
+       // System.out.println(+ marks);
+
+
+       // int a=5;
+       // a--;
+       // System.out.println(-a);
+
+        //module
+        int x=5;
+        System.out.println(x%2);
+
+        int b=6;
+        System.out.println(6*2);
+
+        int c=4;
+        System.out.println(4/2);
+
+        int n=5;
+        System.out.println(n++);
+
+        int m=2;
+        System.out.println(++m);
+
+        int k=8;
+        System.out.println(k--);
+
+        int l=8;
+        System.out.println(--l);
+
+
+        int day=5;
+        switch (day){
+            case 1:
+                System.out.println("mon");
+                break;
+            case 2:
+                System.out.println("tue");
+                break;
+            case 3:
+                System.out.println("wed");
+                break;
+
+            default:
+                System.out.println("invalid");
+
+
+                int button=4;
+                switch (button){
+                    case 1:
+                        System.out.println("name");
+                        break;
+                    case 2:
+                        System.out.println("class");
+                        break;
+                    case 3:
+                        System.out.println("roll no");
+                        break;
+                    case 4:
+                        System.out.println("count");
+                    case 5:
+                        System.out.println("num");
+                        break;
+
+                    default:
+                        System.out.println("invalid");
+
+                }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
