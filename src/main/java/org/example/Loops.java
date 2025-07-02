8d710a271df7c75bc5091cb63c7801f71b4cef2e
Kanchan Kumari <kanchank211195@gmail.com>
2025-07-02 11:53:20 +0530

    Hello

--- src/main/java/org/example/Loops.java
+++ src/main/java/org/example/Loops.java
@@ -20,7 +20,7 @@ public class Loops {
 
          int i=1;
          while(i<=10){
-             System.out.println(i + "Kanchan Kumari");
+             System.out.println(i + "Kanchan Neha");
              i++;
          }
         System.out.println("ydtrdrt");
